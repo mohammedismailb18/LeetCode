0676aed36366208e9da4eb509ded5504ba288848
Mohammed Ismail <58168490+mohammedismailb18@users.noreply.github.com>
2021-06-11 23:27:57 +0530

    Create My_Calendar_I.cpp

---
+++ June LeetCode Challenge 2021/My_Calendar_I.cpp
@@ -0,0 +1,24 @@
+class MyCalendar {
+public:
+    vector<pair<int,int>> interval;
+    int prev;
+    MyCalendar() {
+        prev = -1;
+    }
+    
+    bool book(int start, int end) {
+        sort(interval.begin(), interval.end());
+        for(auto p : interval){
+            if(start < p.second && end > p.first)
+                return false;
+        }
+        interval.push_back({start,end});
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
